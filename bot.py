1fa6a76471f3115a3cfcc6fe3e17d6ceee3c399c
rukiyamuhamed1-cmd <rukiyamuhamed1@gmail.com>
2026-05-14 10:24:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-import fitz  # PyMuPDF
+import fitz
 from telegram import Update
 from telegram.ext import (
     Application,
@@ -11,38 +11,37 @@ from telegram.ext import (
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-# PDF text save
 pdf_text = ""
 
 
-# START COMMAND
+# START
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "PDF Ergi.\n"
-        "PDF erga upload  booda maqaa barreessi."
+        "PDF Ergi.\nPDF erga upload booda maqaa barreessi."
     )
 
 
-# PDF RECEIVE
+# RECEIVE PDF
 async def receive_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global pdf_text
 
-    document = update.message.document
-
-    if not document.file_name.lower().endswith(".pdf"):
-        await update.message.reply_text("PDF file qofa ergi.")
-        return
-
-    file = await context.bot.get_file(document.file_id)
-
-    os.makedirs("downloads", exist_ok=True)
-
-    file_path = f"downloads/{document.file_name}"
-
-    await file.download_to_drive(file_path)
-
     try:
+        document = update.message.document
+
+        if not document.file_name.lower().endswith(".pdf"):
+            await update.message.reply_text("PDF file qofa ergi.")
+            return
+
+        os.makedirs("downloads", exist_ok=True)
+
+        file = await context.bot.get_file(document.file_id)
+
+        file_path = f"downloads/{document.file_name}"
+
+        await file.download_to_drive(custom_path=file_path)
+
         text = ""
+
         pdf = fitz.open(file_path)
 
         for page in pdf:
@@ -60,14 +59,12 @@ async def receive_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f"Dogoggora: {e}")
 
 
-# SEARCH NAME
+# SEARCH
 async def search_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global pdf_text
 
-    if pdf_text == "":
-        await update.message.reply_text(
-            "Dura PDF ergi."
-        )
+    if not pdf_text:
+        await update.message.reply_text("Dura PDF ergi.")
         return
 
     name = update.message.text.strip().lower()
@@ -83,7 +80,6 @@ async def search_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 # MAIN
-
 def main():
     app = Application.builder().token(TOKEN).build()
 
@@ -98,6 +94,7 @@ def main():
     )
 
     print("Bot Running...")
+
     app.run_polling()
 
 
